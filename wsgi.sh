eecbbb354aaa900c4bd38c85f6294d040b9061ba
MichaelShen <michael7105@gmail.comG>
2023-05-05 06:03:06 +0000

    update

---
+++ wsgi.sh
@@ -0,0 +1,14 @@
+if [ "$DOMAIN_NAME" = "" ]
+then
+  echo "\nYou should set your DOMAIN_NAME at first"
+  echo "\ne.g.: export DOMAIN_NAME=\"the_domain_name_you_want\"\n"
+  exit 0
+fi
+
+gunicorn \
+	--workers=4 \
+	--threads=4 \
+	wsgi:app \
+	-b 0.0.0.0:5000 \
+	--certfile="/etc/letsencrypt/archive/$DOMAIN_NAME/fullchain1.pem" \
+	--keyfile="/etc/letsencrypt/archive/$DOMAIN_NAME/privkey1.pem"
