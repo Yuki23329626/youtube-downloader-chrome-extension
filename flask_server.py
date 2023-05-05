bf14e475b1edaf4fe910c66f9213519763b95d12
Michael_Shen <michael7105@gmail.com>
2023-05-05 11:13:07 +0800

    test

--- flask_server.py
+++ flask_server.py
@@ -5,35 +5,36 @@ import os
 
 filename = ''
 
-def my_hook(d):
-    if d['status'] == 'finished':
-        global filename
-        filename = d['filename']
+def main():
+    def my_hook(d):
+        if d['status'] == 'finished':
+            global filename
+            filename = d['filename']
 
-ydl_opts = {
-    'skip_download': False,
-    'writesubtitles': True,
-    'progress_hooks': [my_hook]
-}
+    ydl_opts = {
+        'skip_download': False,
+        'writesubtitles': True,
+        'progress_hooks': [my_hook]
+    }
 
-app = Flask(__name__)
+    app = Flask(__name__)
 
-@app.route('/api/file')
-def get_file():
-    try:
-        link = request.args.get('url')
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download(link)
-        return send_file(filename, as_attachment=True)
-    except Exception as e:
-        return jsonify({'error': str(e)})
+    @app.route('/api/file')
+    def get_file():
+        try:
+            link = request.args.get('url')
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                ydl.download(link)
+            return send_file(filename, as_attachment=True)
+        except Exception as e:
+            return jsonify({'error': str(e)})
 
-# @app.after_request
-# def after_request_func(response):
-#     # Do something after the request has been processed
-#     filename=''
-#     os.remove(filename)
-#     return response
+    # @app.after_request
+    # def after_request_func(response):
+    #     # Do something after the request has been processed
+    #     filename=''
+    #     os.remove(filename)
+    #     return response
 
 if __name__ == '__main__':
     app.run(debug=True)
