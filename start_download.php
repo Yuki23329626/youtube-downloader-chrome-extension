e4bcc19dd9088627b4590d55171d958f734ef7a1
Michael_Shen <michael7105@gmail.com>
2023-05-17 15:20:10 +0800

    test

--- start_download.php
+++ start_download.php
@@ -3,10 +3,12 @@
 $url = 'http://localhost:5000/api/file?url=https://www.youtube.com/watch?v=yLp9x7Dh2jo&format=mp4-1920*1080'; // The URL to send the GET request to
 
 // Get the file contents
-$fileContents = file_get_contents($url);
+// $fileContents = file_get_contents($url);
 $headers = get_headers($url);
 
-echo $header
+foreach ($headers as $header) {
+    echo $header . "\n";
+}
 
 // // Set the HTTP headers
 // header('Content-Type: video/mp4'); // Set the appropriate content type for your video file
