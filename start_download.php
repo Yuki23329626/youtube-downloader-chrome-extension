1b29806670919d769f9c7a1fcf32cb44881e48cc
Michael_Shen <michael7105@gmail.com>
2023-05-17 15:41:23 +0800

    test

--- start_download.php
+++ start_download.php
@@ -4,11 +4,12 @@ $url = 'http://localhost:5000/api/file?url=https://www.youtube.com/watch?v=yLp9x
 
 // Get the file contents
 $fileContents = file_get_contents($url);
+$filename = 'video.mp4';
 
 // Set the HTTP headers
 header('Content-Type: video/mp4'); // Set the appropriate content type for your video file
 header('Content-Length: ' . strlen($fileContents));
-// header('Content-Disposition: attachment;filename="' . $filename . '"'); // Set the desired file name for the downloaded video
+header('Content-Disposition: attachment;filename="' . $filename . '"'); // Set the desired file name for the downloaded video
 
 // Output the file contents
 echo $fileContents;
