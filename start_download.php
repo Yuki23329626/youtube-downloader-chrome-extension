6a43d8b04d2427a3516ef23424e2e47b3490f347
Michael_Shen <michael7105@gmail.com>
2023-05-17 16:27:15 +0800

    test

--- start_download.php
+++ start_download.php
@@ -19,7 +19,7 @@ $headers = get_headers($url, 1);
 if ($headers && strpos($headers[0], '200') !== false) {
     // $content = file_get_contents($url); 
 
-    echo $headers
+    echo $headers[3];
     
     // Print the headers
     // foreach ($headers as $name => $value) {
