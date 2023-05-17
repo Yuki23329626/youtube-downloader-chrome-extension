86c67c53225da1e54b94caeb3cf733ac09a18a76
Michael_Shen <michael7105@gmail.com>
2023-05-17 15:59:03 +0800

    test

--- start_download.php
+++ start_download.php
@@ -5,12 +5,29 @@ $url = 'http://localhost:5000/api/file?url=https://www.youtube.com/watch?v=yLp9x
 // Get the file contents
 // $fileContents = file_get_contents($url);
 
-// Set the HTTP headers
-header('Content-Type: video/mp4'); // Set the appropriate content type for your video file
-header('Content-Length: ' . strlen($fileContents));
-// header('Content-Disposition: attachment;filename="' . $filename . '"'); // Set the desired file name for the downloaded video
+// // Set the HTTP headers
+// header('Content-Type: video/mp4'); // Set the appropriate content type for your video file
+// header('Content-Length: ' . strlen($fileContents));
+// // header('Content-Disposition: attachment;filename="' . $filename . '"'); // Set the desired file name for the downloaded video
 
-// Output the file contents
-echo $url;
+// // Output the file contents
+// echo $fileContents;
+
+$headers = get_headers($url, 1);
+
+// Check if the request was successful (HTTP 200 OK)
+if ($headers && strpos($headers[0], '200') !== false) {
+    $content = file_get_contents($url);
+    
+    // Print the headers
+    foreach ($headers as $name => $value) {
+        echo $name . ': ' . $value . "\n";
+    }
+    
+    // Print the content
+    echo $content;
+} else {
+    echo 'Error: Failed to retrieve file.';
+}
 
 ?>
