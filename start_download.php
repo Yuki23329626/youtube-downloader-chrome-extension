d69faa9fa9bae1bf55f5b56c10dd1faf5ecd8df9
Michael_Shen <michael7105@gmail.com>
2023-05-17 15:34:10 +0800

    test

--- start_download.php
+++ start_download.php
@@ -2,20 +2,33 @@
 
 $url = 'http://localhost:5000/api/file?url=https://www.youtube.com/watch?v=yLp9x7Dh2jo&format=mp4-1920*1080'; // The URL to send the GET request to
 
-// Get the file contents
-$fileContents = file_get_contents($url);
+// // Get the file contents
+// $fileContents = file_get_contents($url);
 
-$filename = $_SERVER['HTTP_CONTENT_DISPOSITION'];
+// // Set the HTTP headers
+// header('Content-Type: video/mp4'); // Set the appropriate content type for your video file
+// header('Content-Length: ' . strlen($fileContents));
+// header('Content-Disposition: attachment;filename="' . $filename . '"'); // Set the desired file name for the downloaded video
 
-echo $filename
+// // Output the file contents
+// echo $fileContents;
 
-// Set the HTTP headers
-header('Content-Type: video/mp4'); // Set the appropriate content type for your video file
-header('Content-Length: ' . strlen($fileContents));
-header('Content-Disposition: attachment;filename="' . $filename . '"'); // Set the desired file name for the downloaded video
+// Make an HTTP request to get the response headers
+$headers = get_headers($url, 1);
 
-// Output the file contents
-echo $fileContents;
+if ($headers !== false && isset($headers['Content-Disposition'])) {
+    $contentDisposition = $headers['Content-Disposition'];
+
+    // Extract the filename from the Content-Disposition header
+    if (preg_match('/filename=(.*)/', $contentDisposition, $matches)) {
+        $filename = trim($matches[1]);
+        echo 'File name: ' . $filename;
+    } else {
+        echo 'Unable to extract the file name';
+    }
+} else {
+    echo 'Failed to retrieve response headers';
+}
 
 
 ?>
