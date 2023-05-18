53cffbc7764d2251e5bc54917ae8dbe7ef817c16
Michael_Shen <michael7105@gmail.com>
2023-05-18 10:25:05 +0800

    test

--- start_download.php
+++ start_download.php
@@ -32,7 +32,7 @@ if ($headers && strpos($headers[0], '200') !== false) {
     // header($headers[4]); // Set the appropriate content type for your video file
     // header($headers[5]);
 
-    Print the headers
+    // Print the headers
     foreach ($headers as $name => $value) {
         echo $name . ': ' . $value . "<BR>";
     }
