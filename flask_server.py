00bb2531a768197e31261778b8b5fb225c228e03
Michael_Shen <michael7105@gmail.com>
2023-05-18 13:45:19 +0800

    test

--- flask_server.py
+++ flask_server.py
@@ -79,7 +79,7 @@ def get_file():
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download(link)
         list_files = glob.glob(filename + '*')
-        # print('filename: ', filename)
+        print('filename: ', filename)
 
         # @after_this_request
         def after_request(response):
