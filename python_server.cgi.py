79c65a062d3fd1b89053bf256c1e12f05515a454
Michael_Shen <michael7105@gmail.com>
2023-06-07 17:12:18 +0800

    test

---
+++ python_server.cgi.py
@@ -0,0 +1,148 @@
+from flask import Flask, jsonify, send_file, request, after_this_request, make_response
+import yt_dlp
+import logging
+import glob
+import os
+import time
+from threading import Thread
+import re
+import cgi
+
+filename = ''
+SAVE_PATH = 'cache/'
+# print('SAVE_PATH', SAVE_PATH)
+
+logging.basicConfig(level=logging.ERROR,
+                    format='%(levelname)-8s %(asctime)s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    handlers=[logging.FileHandler(SAVE_PATH + 'ytdlp_lite.log', 'w', 'utf-8'),])
+
+# get the final filename after downloading the file
+
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        global filename
+        # filename = d['info_dict']['title']
+        filename = d['filename'].split('.')[0]
+
+
+# options for the yt-dlp(github project)
+ydl_opts = {
+    # 'format': 'bestaudio/best',
+    # 'postprocessors': [{
+    #     'key': 'FFmpegExtractAudio',
+    #     'preferredcodec': 'mp3',
+    #     'preferredquality': '192',
+    # }],
+    # 'skip_download': False,
+    'writesubtitles': False,
+    'progress_hooks': [my_hook],
+    'outtmpl': SAVE_PATH + '%(title)s.%(ext)s',
+    'nooverwrites': False
+}
+
+# app = Flask(__name__)
+
+# Remove downloaded file after serve the target file to the client
+
+def remove_file(file):
+    time.sleep(2)
+    while glob.glob(file):
+        try:
+            print('removing files:', file)
+            os.remove(glob.glob(file)[0])
+        except Exception as e:
+            print(e)
+
+# def remove_file(file):
+#     while True:
+#         time.sleep(2)
+#         try:
+#             print('try remove file:', file)
+#             os.remove(file)
+#         except Exception as e:
+#             if not os.path.exists(file):
+#                 break
+#             print(e)
+
+# Create an instance of the FieldStorage class to parse the CGI request
+form = cgi.FieldStorage()
+# Get the value of the "name" parameter from the GET request
+link = form.getvalue('url')
+file_format = form.getvalue('format')
+
+# choose the file format you want, some version of python3 cannot use match function
+if file_format == 'mp4-1920*1080':
+    print('file_format', file_format)
+    ydl_opts['format'] = 'bestvideo[height<=1080][ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best'
+elif file_format == 'bestaudio':
+    ydl_opts['format'] = 'bestaudioo/best'
+    ydl_opts['postprocessors'] = [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192'
+    }]
+
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(link)
+list_files = glob.glob(filename + '*')
+# print('list_files: ', list_files)
+
+# @after_this_request
+# def after_request(response):
+#     t = Thread(target=remove_file, args=(list_files[0],))
+#     t.start()
+#     return response
+
+# filename_ = re.split(r"[/\\]",list_files[0])[-1]
+# return send_from_directory(directory=SAVE_PATH, filename=filename_)
+
+# Create a response object
+# response = make_response(send_file(list_files[0], as_attachment=True))
+
+# Add custom attributes to the response headers
+filename_ = re.split(r"[/\\]",list_files[0])[-1]
+print("Content-Disposition: attachment; filename=" + filename_)
+
+if file_format == 'bestaudio':
+    print("Content-type: audio/mpeg")
+
+# Read the video file content and send it to the client
+with open(list_files[0], 'rb') as file:
+    content = file.read()
+    print(content)
+        
+remove_file(list_files[0])
+
+# @app.route('/api/file')
+# def get_file():
+#     parameters = request.args.to_dict()
+#     link = parameters.pop('url')
+#     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+#         ydl.download(link)
+#     list_files = glob.glob(filename + '*')
+#     response = send_file(SAVE_PATH + '\\' + list_files[0])
+#     @after_this_request
+#     def remove_file(response):
+#         print('File download complete')
+#         os.remove(SAVE_PATH + '\\' + list_files[0])
+#         return response
+#     return response
+
+# @app.after_request
+# def after_request_func(response):
+#     # Do something after the request has been processed
+#     list_files = glob.glob(filename + '*')
+#     while True:
+#         time.sleep(5)
+#         try:
+#             os.remove(list_files[0])
+#             break
+#         except:
+#             pass
+#     return response
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True, host="0.0.0.0")
