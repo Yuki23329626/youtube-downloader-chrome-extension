35126161a8016d1e5497025faab2c67a0dbd0ce0
michael_shen <michael7105@gmail.com>
2023-08-22 00:50:26 +0800

    test

--- flask_server.py
+++ flask_server.py
@@ -73,8 +73,8 @@ def get_file():
     if file_format == 'mp4-1920*1080':
         print('file_format', file_format)
         # ydl_opts['format'] = 'bestvideo[height<=1080][ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best'
-        ydl_opts['format'] = 'bestvideo*[ext=mp4]+bestaudio[ext=m4a]'
-        # ydl_opts['format'] = 'b[ext=mp4]'
+        # ydl_opts['format'] = 'bestvideo*[ext=mp4]+bestaudio[ext=m4a]'
+        ydl_opts['format'] = 'bv[ext=mp4]'
     elif file_format == 'bestaudio':
         ydl_opts['format'] = 'bestaudio[ext=webm]'
         # ydl_opts['postprocessors'] = [{
