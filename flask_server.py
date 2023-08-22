3e5e802f9a9480b1b77dbfcfd8b7a2f519fd2146
Michael_Shen <michael7105@gmail.com>
2023-08-22 11:06:08 +0800

    test

--- flask_server.py
+++ flask_server.py
@@ -75,7 +75,7 @@ def get_file():
         # ydl_opts['format'] = 'bestvideo[height<=1080][ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best'
         ydl_opts['format'] = 'bestvideo*[ext=mp4]+bestaudio[ext=m4a]'
     elif file_format == 'bestaudio':
-        ydl_opts['format'] = 'bestaudio[ext=mp3]'
+        ydl_opts['format'] = 'bestaudio[ext=mp4]'
         # ydl_opts['postprocessors'] = [{
         #     'key': 'FFmpegExtractAudio',
         #     'preferredcodec': 'mp3',
