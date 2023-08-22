c04c49d01fbf5cc65eabc6d9f2c9a2bc0de8d8d6
Michael_Shen <michael7105@gmail.com>
2023-08-22 11:33:54 +0800

    test

--- start_download.php
+++ start_download.php
@@ -37,9 +37,7 @@ if ($headers && strpos($headers[0], '200') !== false) {
     header($headers[5]);
     header($headers[3]); // Set the desired file name for the downloaded video
 
-    // echo $fileContents;
-    $file = $host . 'youtube-downloader-chrome-extension/cache/' . $headers[3];
-    readfile($fileContents)
+    echo $fileContents;
 } else {
     echo 'Error: Failed to retrieve file.';
 }
