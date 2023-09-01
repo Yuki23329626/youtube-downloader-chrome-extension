859654888f34595cae6b94f7d01c215590ccd8b3
Michael Shen <michael7105@gmail.com>
2023-09-01 05:36:59 +0000

    update shell script

--- kill_gunicorn.sh
+++ kill_gunicorn.sh
@@ -1,5 +1,12 @@
-gunicorn_id=`ps -aux | grep gunicorn | grep -v "grep" | head -n 1 | awk '{print $2}'`
+gunicorn_id=`ps -aux | grep gunicorn | grep -v "grep" | grep -v "kill" | head -n 1 | awk '{print $2}'`
 
-kill $gunicorn_id
+ps -aux | grep gunicorn
+
+echo gunicorn_id = "$gunicorn_id"
+
+if [ "$gunicorn_id" ]; then
+	kill $gunicorn_id
+	echo "kill $gunicorn_id"
+fi
 
 exit 0
