12aff00cf5d159116574f04484bfc8ea666d2fc3
Michael_Shen <michael7105@gmail.com>
2023-09-04 15:32:09 +0800

    test

--- flask_server.py
+++ flask_server.py
@@ -98,7 +98,7 @@ async def get_file():
     absolute_path = os.path.abspath(relative_path)
     list_files = [file for file in os.listdir(absolute_path) if file.startswith(prefix)]
 
-    logging.debug('list_files:', list_files)
+    logging.info('list_files:', list_files)
     
     @after_this_request
     def after_request(response):
