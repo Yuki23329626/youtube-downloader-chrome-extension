2c44acda3896849b1ffc2837cfee23b045f77664
Michael_Shen <michael7105@gmail.com>
2023-09-04 15:43:15 +0800

    test

--- flask_server.py
+++ flask_server.py
@@ -17,9 +17,8 @@ if not os.path.exists(SAVE_PATH):
 
 logging.basicConfig(level=logging.INFO,
                     format='%(levelname)-8s %(asctime)s %(message)s',
-                    mode='a',
                     datefmt='%Y-%m-%d %H:%M:%S',
-                    handlers=[logging.FileHandler(SAVE_PATH + 'log.ytdlp_lite', 'w', 'utf-8'),])
+                    handlers=[logging.FileHandler(SAVE_PATH + 'log.ytdlp_lite', 'w', 'utf-8',mode='a'),])
 
 # get the final filename after downloading the file
 
