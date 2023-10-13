bd7c3ba368019c06ea838a16687dfc5460c52db2
Michael_Shen <michael7105@gmail.com>
2023-10-13 15:30:05 +0800

    test

--- react-app/src/MyForm.js
+++ react-app/src/MyForm.js
@@ -41,44 +41,36 @@ class MyForm extends Component {
       // You can add your save logic here
 
       // Build the URL with parameters
-      const apiUrl = `/api/file?v=${v}&format=bestaudio`; // Replace with your API endpoint and parameters
+      const apiUrl = `http://localhost:5000/api/file?v=${v}&format=bestaudio`; // Replace with your API endpoint and parameters
       this.setState({ full_request_url: apiUrl });
 
-      fetch(apiUrl, {
-        method: 'GET',
-        responseType: 'blob', // Specify that the response is binary data
-      })
-        .then(response => {
-          const contentDispositionHeader = response.headers.get('content-disposition');
-          let filename = 'log_download.log'; // Default filename
+      useEffect(() => {
+        axios.get(apiUrl, { responseType: 'blob' })
+          .then(response => {
+            // Extract the filename from the response headers
+            const filename = response.headers['content-disposition']
+              .split(';')
+              .find(param => param.trim().startsWith('filename='))
+              .split('=')[1];
 
-          if (contentDispositionHeader) {
-            const filenameMatch = /filename=([^;]+)/.exec(contentDispositionHeader);
-            if (filenameMatch && filenameMatch[1]) {
-              filename = filenameMatch[1];
-            }
-          }
+            // Create a Blob from the response data
+            const blob = new Blob([response.data]);
 
-          return response.blob()
-            .then(blob => {
-              // Create a URL for the binary data
-              const url = window.URL.createObjectURL(blob);
+            // Create an object URL for the Blob
+            const url = window.URL.createObjectURL(blob);
 
-              // Create a temporary anchor element to trigger the download
-              const a = document.createElement('a');
-              a.href = url;
-              a.download = filename;
+            // Create a temporary anchor element to trigger the download
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = filename; // Set the filename
 
-              // Trigger a click event on the anchor to initiate the download
-              a.click();
+            // Trigger a click event on the anchor to initiate the download
+            a.click();
 
-              // Revoke the object URL to free up resources
-              window.URL.revokeObjectURL(url);
-            });
-        })
-        .catch(error => {
-          console.error('Error:', error);
-        });
+            // Revoke the object URL to free up resources
+            window.URL.revokeObjectURL(url);
+          })
+      });
 
     } catch (error) {
       console.error('Error downloading file:', error);
