97dcc737398e412ea4f9e0178e4c31071b0ce75d
Michael_Shen <michael7105@gmail.com>
2023-10-13 15:13:30 +0800

    test

--- react-app/src/MyForm.js
+++ react-app/src/MyForm.js
@@ -41,7 +41,7 @@ class MyForm extends Component {
       // You can add your save logic here
 
       // Build the URL with parameters
-      const apiUrl = `/api/file?v=${v}&format=bestaudio`; // Replace with your API endpoint and parameters
+      const apiUrl = `http://localhost:5000/api/file?v=${v}&format=bestaudio`; // Replace with your API endpoint and parameters
       this.setState({ full_request_url: apiUrl });
 
       fetch(apiUrl, {
