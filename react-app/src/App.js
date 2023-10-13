9666ab4aaaf348bd6dab34bea4164ec99a396c9d
michael_shen <michael7105@gmail.com>
2023-10-13 20:30:25 +0800

    test

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -42,7 +42,7 @@ function App() {
       <img src={logo} className="App-logo" alt="logo" />
       <br />
 
-      <div className='fill-page'>
+      <div className='fill-screen'>
         <MyForm></MyForm>
       </div>
 
