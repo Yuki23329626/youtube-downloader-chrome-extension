7464379db21007e87bee7fa61ecbdf0dd472ba2f
michael_shen <michael7105@gmail.com>
2023-10-13 21:03:14 +0800

    test

--- react-app/src/MyForm.js
+++ react-app/src/MyForm.js
@@ -207,7 +207,7 @@ class MyForm extends Component {
             onChange={this.handleInputChange}
           />
         </label>
-        <button onClick={this.paste} style={{ verticalAlign: 'center'}}>Paste</button>
+        <button onClick={this.paste} style={{ verticalAlign: 'middle'}}>Paste</button>
         <br />
         <button onClick={this.clickDownloadAudio}>Download Audio</button>
         <button onClick={this.clickDownloadVideo}>Download Video</button>
